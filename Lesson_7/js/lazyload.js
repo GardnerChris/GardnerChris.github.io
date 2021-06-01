ff309a2d73976ecdf8f1f8a006b0dc3f57a4be25
GardnerChris <82838410+GardnerChris@users.noreply.github.com>
2021-06-01 14:41:39 -0600

    Update lazyload.js

--- Lesson_7/js/lazyload.js
+++ Lesson_7/js/lazyload.js
@@ -26,32 +26,3 @@ images.forEach(image => {
 })
 
 
-/*const images = document.querySelectorAll("img[data-src]");
-
-function preloadImage (img){
-    const src=img.getAttribute("data-src");
-    if (!src){
-        return
-    }
-    img.src=src;
-}
-    const imgOptions= {
-        threshold:0,
-        rootMargin:"0px 0px 60px 0px"};
-
-const imgObserver= new IntersectionObserver((entries,imgObserver) =>{
-    entries.forEach(entry =>{
-        if(!entry.isIntersecting){
-            return;
-        }
-        else{
-            preloadImage(entry.target);
-            imgObserver.unobserve(entry.target);
-        }
-    })
-},imgOptions);
-
-
-images.forEach(image =>{
-    imgObserver.observe(image);
-});
