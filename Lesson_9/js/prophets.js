d78042c25a6edf918b4558b2f2157e686c91265f
GardnerChris <82838410+GardnerChris@users.noreply.github.com>
2021-06-17 21:53:34 -0600

    Add files via upload

--- Lesson_9/js/prophets.js
+++ Lesson_9/js/prophets.js
@@ -1,5 +1,6 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
+/*-------------------JSON------------------------- */
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 fetch(requestURL)
   .then(function (response) {
     return response.json();
@@ -8,19 +9,23 @@ fetch(requestURL)
     console.table(jsonObject);  // temporary checking for valid response and data parsing
     const prophets = jsonObject['prophets'];
 
-
     for (let i = 0; i < prophets.length; i++ ) {
 
+      /*create variables*/
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
       let birthDate=document.createElement("p");
       let birthPlace= document.createElement("p");
       let image=document.createElement("img");
-      image.setAttribute('src', prophets[i].imageurl);
       let date=document.createElement("birthdate");
+      image.setAttribute('src', prophets[i].imageurl);
+
+      /*call and store information in variables*/
       h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
       birthDate.textContent="Date of Birth: " + prophets[i].birthdate;
       birthPlace.textContent= "Place of Birth: " + prophets[i].birthplace;
+      
+      /*append node*/
       card.appendChild(h2);
       card.appendChild(birthPlace);
       card.appendChild(birthDate);
@@ -31,29 +36,4 @@ document.querySelector('div.cards').appendChild(card);
     
   });
 
-  
-
-
-
-
-
-
-
-
-
- /* 
-      let card = document.createElement('section');
-let h2 = document.createElement('h2');
-let p=document.createElement("p");
-let image=document.createElement("image");
-        
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-    
-card.appendChild(h2);
-card.appendChild(p);
-card.appendChild(image);    
-document.querySelector('div.cards').appendChild(card);
-image.setAttribute('src', prophets[i].imageurl);
-const prophets = jsonObject['prophets'];   
-*/
  
